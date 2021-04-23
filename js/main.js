92ecbfd97fc2d16a2a4597b1eb2bbd61a02cbcdc
takuya805 <takuya0903exile@gmail.com>
2021-04-23 23:54:10 +0900

    create-view

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-var app= new({
-  al: '#app'
+var app= new Vue({
+  el: '#app',
+  data:{
+    bpi: null
+  },
+  mounted: function(){
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function(response){
+      this.bpi=response.data.bpi
+    }.bind(this))
+    .catch(function(error){
+      console.log(error)
+    })
+  }
 })
